b56b7bdf25a073ab0df4f619f9e23d028ab21486
Pāvels Ivanovs <117778903+SurferFromOviedo@users.noreply.github.com>
2023-02-18 19:38:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,21 +14,21 @@ def find_mismatch(text):
             opening_brackets_stack.append(Bracket(next, i + 1))
         elif next in ")]}":
             if len(opening_brackets_stack) == 0:
-                return Bracket(next, i + 1)
+                return Bracket(next, i + 1).position
             for element in reversed(opening_brackets_stack):
                 if are_matching(element.char, next):
                     opening_brackets_stack.pop()
                     break
-                else: return Bracket(next, i + 1)
+                else: return Bracket(next, i + 1).position
     if len(opening_brackets_stack) == 1:
-        return opening_brackets_stack[0]
-
+        return opening_brackets_stack[0].position
+    if len(opening_brackets_stack) == 0:
+        return "Success"
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    if mismatch:
-        print(mismatch.position)
-    else: print("Success")
+    print(mismatch)
+
 
 
 if __name__ == "__main__":
