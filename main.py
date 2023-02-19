7d2fed9b7cb5efa129ad256415beafee12cf25e2
Pāvels Ivanovs <117778903+SurferFromOviedo@users.noreply.github.com>
2023-02-19 21:06:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,24 +12,23 @@ def find_mismatch(text):
             opening_brackets_stack.append(Bracket(next, i + 1))
         elif next in ")]}":
             if len(opening_brackets_stack) == 0:
-                return Bracket(next, i + 1).position
+                return Bracket(next, i + 1).position -1
             for element in reversed(opening_brackets_stack):
                 if are_matching(element.char, next):
                     opening_brackets_stack.pop()
                     break
-                else: return Bracket(next, i + 1).position
+                else: return Bracket(next, i + 1).position -1
     if len(opening_brackets_stack) == 1:
-        return opening_brackets_stack[0].position
+        return opening_brackets_stack[0].position - 1
     if len(opening_brackets_stack) == 0:
         return "Success"
-    else: return opening_brackets_stack[0].position
+    else: return opening_brackets_stack[0].position - 1
 
 def main():
-    text = ""
     text1 = input()
-    if text1 == "I":
-        text = input()
-    elif text1 == "F":
+    if text1[0] == "I":
+        text = text1[1:]
+    elif text1[0] == "F":
         file_path = input()
         file = open(file_path, "r")
         text_file = file.readlines()
